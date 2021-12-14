07edf50cae48482a11bfe9ea4621664ad0df7034
chaijun <562563909@qq.com>
2021-12-14 14:03:30 +0800

    update DappCryptoUtil.java

--- nft-api/fingernft-api/src/main/java/com/fingerchar/api/utils/DappCryptoUtil.java
+++ nft-api/fingernft-api/src/main/java/com/fingerchar/api/utils/DappCryptoUtil.java
@@ -9,6 +9,7 @@ import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 import org.web3j.abi.DefaultFunctionEncoder;
 import org.web3j.abi.TypeEncoder;
 import org.web3j.abi.datatypes.Address;
